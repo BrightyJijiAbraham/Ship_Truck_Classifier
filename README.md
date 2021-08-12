c87cda5bfeec627b1927b2271a89c197c6fa10cd
BrightyJijiAbraham <brightymct@gmail.com>
2021-08-12 07:51:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Ship Truck Classifier
+A Machine Learning program to identify Ship and Truck<br>
+
+## How it Works ?
+<p>
+Step 1: open the app in web<br>
+Step 2: click choose file<br>
+step 3: choose the image you want to predict<br>
+step 4: click submit<br>
+step 5: wait for some time until the output appears below the submit form and the uploaded image is loaded in the background<br>
+</p>
+
+## Libraries used
+<p>
+Flask<br>
+Tensorflow<br>
+Keras<br>
+matplotlib<br>
+sys<br>
+os<br>
+seaborn<br>
+cv2<br>
+numpy<br>
+sklearn<br>
+</p>
+
+## How to configure
+<p>
+step 1: install <a href="https://www.python.org/"> python </a><br>
+step 2: create a new virtual environment in the project folder (<a href="https://www.liquidweb.com/kb/how-to-setup-a-python-virtual-environment-on-windows-10/"> how to ?</a>)<br>
+step 3: activate the environment <br>
+step 4: install the required libraries mentioned above (<a href="https://packaging.python.org/tutorials/installing-packages/#use-pip-for-installing"> how to ?</a>)<br>
+</p>
+
+## How to Run
+<p>
+step 1: open cmd<br>
+step 2: open the file directory in cmd<br>
+step 3: activate the virtual environment<br>
+step 4: run the app.py file (using command: python app.py)<br>
+step 5: open the app in web browser by using the address shown in cmd after running step 4<br>
+</p>
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array, load_img
+sys.path.append(os.path.abspath('./model'))
+from keras.models import load_model
+
+
+app = Flask(__name__)
+model = load_model('model.h5')
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def prediction():
+    imagefile= request.files['imagefile']
+    classification = ['','']
+    classification[0]= "images/"+ imagefile.filename
+    image_path = "./static/images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    image1 = load_img(image_path, target_size=(224, 224))##loading the image
+    
+    
+    image1 = np.asarray(image1) ##converting to an array
+    image1 = image1 / 255 ##scaling by doing a division of 255
+    plt.imshow(image1)
+    image1 = np.expand_dims(image1, axis=0) ##expanding the dimensions
+    output = model.predict(image1)
+    if ((output[0][0]>0.8000) or (output[0][1]>0.9000)):
+        if output[0][0] > output[0][1]:
+            classification[1] = "Image is of a ship"##ship
+        else:
+            classification[1] = "Image is of a truck"##truck
+    else:
+        if output[0][0] > output[0][1]:
+            classification[1] = "Sorry I can't recognise this image but it can be " + str(round((output[0][0]/1)*100)) + "% ship"##ship
+        else:
+            classification[1] = "Sorry I can't recognise this image but it can be " + str(round((output[0][1]/1)*100)) + "% truck"##truck
+    return render_template('index.html', prediction=classification)
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
